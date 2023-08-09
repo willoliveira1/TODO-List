4955e03c8dad6a66a8afb4f394d0dd7db63d5131
willoliveira1 <will.oliveira1@hotmail.com>
2023-08-09 20:51:19 -0300

    [FEAT] Adicionado as classes para persistencia de dados.
    Add:
        + Adicionado interface Processor.
        + Adicionado classe TasksProcessor.
        + Adicionado classe CategoriesProcessor.
        + Adicionado metodos e outro construtor para Task .
        + Adicionado metodos para Board.

--- src/main/java/br/com/acelerazg/todolist/ApiApplication.java
+++ src/main/java/br/com/acelerazg/todolist/ApiApplication.java
@@ -1,8 +1,39 @@
 package br.com.acelerazg.todolist;
 
+import br.com.acelerazg.todolist.domain.Category;
+import br.com.acelerazg.todolist.domain.Priority;
+import br.com.acelerazg.todolist.domain.Status;
+import br.com.acelerazg.todolist.domain.Task;
+import br.com.acelerazg.todolist.infra.fileProcessors.CategoriesProcessor;
+import br.com.acelerazg.todolist.infra.fileProcessors.TasksProcessor;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
 public class ApiApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        CategoriesProcessor cp = new CategoriesProcessor("src/main/resources/categories.csv");
+
+        Category category = new Category("Categoria 5");
+        cp.writeFile(category);
+
+        for (Category cat : cp.readFile()) {
+            System.out.println(cat);
+        }
+
+
+        TasksProcessor tp = new TasksProcessor("src/main/resources/tasks.csv");
+
+        Task task = new Task(
+                "Task 6",
+                "Descricao 6",
+                Status.DONE,
+                new Category("Categoria 2"),
+                Priority.URGENT
+        );
+        tp.writeFile(task);
 
 
     }
---
+++ src/main/java/br/com/acelerazg/todolist/infra/BoardService.java
@@ -0,0 +1,6 @@
+package br.com.acelerazg.todolist.infra;
+
+public class BoardService {
+
+
+}
